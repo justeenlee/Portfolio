f1b4bfda09cde5ca22c90c1ae814e51f55b0a7b7
Justeen Lee <justeenlee@gatech.edu>
2015-10-24 13:45:25 -0400

    Update Resume

--- js/overall.js
+++ js/overall.js
@@ -1,4 +1,6 @@
 var navLinkArray = ["link-home", "link-projects", "link-resume", "link-about"];
+var navItemArray = ["justeenlee.com","projects","resume","about"];
+
 
 var currentPath = window.location.pathname.split("/")[(window.location.pathname.split("/").length)-2];
 
@@ -7,9 +9,10 @@ var currentPath = window.location.pathname.split("/")[(window.location.pathname.
 (function(){
     move("currentPath");
     setFullWidth();
-    if (currentPath != "about"){
-        navCheck();
-    }
+    navCheck();
+//    if (currentPath != "about"){
+//        navCheck();
+//    }
 })();
 
 
@@ -17,6 +20,10 @@ $(window).scroll(function (){
     if (currentPath != "about"){
         navCheck();
     }
+
+    
+    
+    console.log(currentPath);
 });
 
 $(window).resize(function(){
@@ -68,26 +75,85 @@ function move(input){
     }
 }
 
+//1022 backup navCheck()
+//function navCheck(){
+//    var currentViewTopEdgePosition = $(window).scrollTop();
+//    
+//    if (currentViewTopEdgePosition > 625){
+//        for (var i=0; i<navLinkArray.length; i++){
+//            var linkName = "#"+navLinkArray[i];
+//            $(linkName).css("color", "#555");
+//        }
+//        $("#indicator-dot").css("backgroundColor", "#555");
+//        $("#cf").css("opacity", ".7");
+//    } else {
+//         for (var i=0; i<navLinkArray.length; i++){
+//            var linkName = "#"+navLinkArray[i];
+//            $(linkName).css("color", "#fff");
+//        }
+//        $("#indicator-dot").css("backgroundColor", "#fff");
+//        $("#cf").css("opacity", ".1");
+//    }
+//
+//}
+
+
+
+//function navCheck(){
+//    var toChangeNavColor = false;
+//    var counter = 0;
+//    for (var i=0; i<navItemArray.length; i++){
+//        if (currentPath != navItemArray[i]){
+//           counter ++;
+//        }
+//    }
+//    if (counter == 4){toChangeNavColor=true;}    
+//        if (toChangeNavColor){    
+//        var currentViewTopEdgePosition = $(window).scrollTop();
+//        if (currentViewTopEdgePosition > 625){
+//            setDefaultNavColor();
+//        } else {
+//            for (var i=0; i<navLinkArray.length; i++){
+//                var linkName = "#"+navLinkArray[i];
+//                $(linkName).css("color", "#fff");
+//            }
+//            $("#indicator-dot").css("backgroundColor", "#fff");
+//            $("#cf").css("opacity", ".1");
+//        }
+//    }
+//
+//}
+
 function navCheck(){
-    var currentViewTopEdgePosition = $(window).scrollTop();
-    
-    if (currentViewTopEdgePosition > 625){
-        for (var i=0; i<navLinkArray.length; i++){
-            var linkName = "#"+navLinkArray[i];
-            $(linkName).css("color", "#555");
+    var toChangeNavColor = false;
+    var i = 0;
+    //if equal, then fase
+    while(currentPath!=navItemArray[i]){
+        
+    }
+        if (toChangeNavColor){    
+        var currentViewTopEdgePosition = $(window).scrollTop();
+        if (currentViewTopEdgePosition > 625){
+            setDefaultNavColor();
+        } else {
+            for (var i=0; i<navLinkArray.length; i++){
+                var linkName = "#"+navLinkArray[i];
+                $(linkName).css("color", "#fff");
+            }
+            $("#indicator-dot").css("backgroundColor", "#fff");
+            $("#cf").css("opacity", ".1");
         }
-        $("#indicator-dot").css("backgroundColor", "#555");
-        $("#cf").css("opacity", ".7");
-    } else {
-         for (var i=0; i<navLinkArray.length; i++){
-            var linkName = "#"+navLinkArray[i];
-            $(linkName).css("color", "#fff");
-        }
-        $("#indicator-dot").css("backgroundColor", "#fff");
-        $("#cf").css("opacity", ".1");
     }
 
 }
 
 
-
+//only each project is set here, else set in css
+function setDefaultNavColor(){
+     for (var i=0; i<navLinkArray.length; i++){
+            var linkName = "#"+navLinkArray[i];
+            $(linkName).css("color", "#555");
+        }
+        $("#indicator-dot").css("backgroundColor", "#555");
+        $("#cf").css("opacity", ".7");
+}
