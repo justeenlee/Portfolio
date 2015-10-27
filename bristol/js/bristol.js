b5b92d59f508fa610057ff6125d08f8504013641
Justeen Lee <justeenlee@gatech.edu>
2015-10-27 01:05:40 -0400

    Update Bristol

--- bristol/js/bristol.js
+++ bristol/js/bristol.js
@@ -2,6 +2,7 @@ $(window).bind("load",function() {setTimeout(addVideo, 1400);});
 
 function addVideo(){
 	document.getElementById('bristol-overview').innerHTML= '<iframe src="http://player.vimeo.com/video/142019964?byline=0&amp;portrait=0&amp;color=eee&amp;rel=0;" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+    document.getElementById('bristol-instruction').innerHTML= '<iframe src="http://player.vimeo.com/video/143702916?byline=0&amp;portrait=0&amp;color=eee&amp;rel=0;" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 }
 
 $(window).scroll(function (){
