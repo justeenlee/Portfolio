95e6c38594fdb75b4b03cd419c229641cfb883d5
Justeen Lee <justeenlee@gatech.edu>
2015-12-14 19:55:48 -0500

    Update Projects

--- projects/js/projects.js
+++ projects/js/projects.js
@@ -1,23 +1,23 @@
 var fadeInSpeed = 500;
-var fadeOutSpeed = 650;
+var fadeOutSpeed = 500;
 var projects = new Array(
-	"#project-crown",
-	"#project-christmas-design",
-	"#project-bridge",
-	"#project-parsons",
-	"#project-ixda",
-	"#project-expression",
-	"#project-motion",
-	"#project-archive",
-	"#project-turo",
-	"#project-symbio",
-	"#project-native",
-	"#project-steam",
-	"#project-soaphero",
-	"#project-lift",
-	"#project-drive",
-	"#project-calor",
-	"#project-bristol"
+	"project-crown",
+	"project-christmas-design",
+	"project-bridge",
+	"project-parsons",
+	"project-ixda",
+	"project-expression",
+	"project-motion",
+	"project-archive",
+	"project-turo",
+	"project-symbio",
+	"project-native",
+	"project-steam",
+	"project-soaphero",
+	"project-lift",
+	"project-drive",
+	"project-calor",
+	"project-bristol"
 );
 
 var links = new Array(
@@ -34,53 +34,21 @@ var links = new Array(
 	"../calor/calor.html"
 );
 
-$(document).ready(
-	hoverLoop()
-);
 
-
-function hoverLoop(){
-	for (var i = 0; i < projects.length; i++) {	$(projects[i]).mouseover(fadeImage(projects[i]));
-	}
-};
-
-function fadeImage(projectName){
-	$(projectName).hover(
-		function() {
-			$(projectName).stop().fadeTo(fadeInSpeed, 0);
-		},
-		function() {
-			$(projectName).stop().fadeTo(fadeOutSpeed,1);
-		}
-	);
-};
-
-
-
-function move(input){
-    var dot = document.getElementById("indicator-dot");
-    //for setting up dot position after page loaded
-    
-    if (input == "currentPath"){
-        var input = currentPath;
-    }
-
-    if (window.outerWidth > 680){
-        if(input == "index"){
-            //dot.style.left = "210px";
-            dot.style.left = "18%";
-        } else if (input == "resume"){
-            //dot.style.left = "552px";
-            dot.style.left = "65%";
-        } else if (input == "about"){
-            //dot.style.left = "695px";
-            dot.style.left = "85%";
-        } else {
-            //dot.style.left = "395px";
-            dot.style.left = "43.5%";
-        }   
+function focusOn(obj){
+    for (i=0; i<projects.length; i++){
+        if (projects[i] != obj.id){
+            $("#"+projects[i]).stop().fadeTo(fadeOutSpeed,0);
+        }
     }
 }
+
+function focusOut(){
+    for (i=0; i<projects.length; i++){
+         $("#"+projects[i]).stop().fadeTo(fadeOutSpeed,1);
+    }
+}
+
 
 function getRandom(){
     var num = Math.floor(Math.random() * 11);
